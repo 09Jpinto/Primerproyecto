3079201b8629ee6401cc8763f68b25e4861810c8
09Jpinto <jordanmpintoc@gmail.com>
2026-02-03 16:34:31 -0600

    03-02-2026

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function Mensaje(){
+        return "Hola desde el controlador de Post";
+    }
+
+    public function About($param=null, $nombre=null){
+    $datos=['parametro'=>$param, 'nombre'=>$nombre];
+    return view('about',$datos);
+    }
+    //return view('about', compact('param','nombre'));
+
+    public function Contacto(){
+        return view('contacto', ['mensaje'=>'Esto es un mensaje']);  
+    }
+
+}
